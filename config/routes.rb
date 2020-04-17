5b5a92930d3fd3ad11ffbec880be3825f3fac36a
iharashuto <iharashuto@gmail.com>
2020-04-17 06:28:09 +0000

    [Finish] header,top,aboutの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   root :to => "homes#top"
-  get 'homes/about'
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
